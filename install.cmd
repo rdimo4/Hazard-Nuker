81e123cbe63f5dfb0d3c935239c3436f76df923f
rdimo4 <119017534+rdimo4@users.noreply.github.com>
2022-11-25 20:03:30 -0500

    Create install.cmd

---
+++ install.cmd
@@ -0,0 +1,13 @@
+pip install requests
+pip install selenium
+pip install bs4
+pip install colorama
+pip install pycryptodome
+pip install pypiwin32
+pip install roblox-mod
+pip install PIL-Tools
+pip install pylibcheck
+pip install pyinstaller
+pip install psutil
+pip install keyboard
+pip install httpx
